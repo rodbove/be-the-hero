1dc2156833659a1ae59fc66fb856884adee0bb0a
Rodrigo Brochado <41601403+PooWoox@users.noreply.github.com>
2020-03-25 18:26:44 -0300

    Added port to work on GCloud

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+let port = process.env.PORT || 8080
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
